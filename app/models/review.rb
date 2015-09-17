23261a4d3462ffd88d22bf7da23ff2c9d7ae414d
xyzzyz <xyzdclxvi@gmail.com>
2015-09-17 17:49:58 +0200

    review spec ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+  
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :user_id, presence: true
 end
