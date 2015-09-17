84190f62276625f9f8430192a76d570cd509c994
xyzzyz <xyzdclxvi@gmail.com>
2015-09-17 15:35:58 +0200

    product spec ok

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, format: /\A\d+(?:\.\d{0,2})?\z/
+  
+  def average_rating
+    revs = self.reviews
+    revs.inject(0.0) { |sum,r| sum += r.rating } / revs.count
+  end
 end
